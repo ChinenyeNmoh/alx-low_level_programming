21dea7927dfa7eb9f59963e166355f4081ba917c
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-02-16 06:41:18 -0800

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-printf("Size of a char: %lu byte(s)", (unsignedlong)sizeof(a));
-printf("Size of a int: %lu byte(s)", (unsignedlong)sizeof(b));
-printf("Size of a long int: %lu byte(s)", (unsignedlong)sizeof(c));
-printf("Size of a long long int: %lu byte(s)", (unsignedlong)sizeof(d));
-printf("Size of a float: %lu byte(s)", (unsignedlong) sizeof(e));
+printf("Size of a char: %lu byte(s)", sizeof(a));
+printf("Size of a int: %lu byte(s)", sizeof(b));
+printf("Size of a long int: %lu byte(s)", sizeof(c));
+printf("Size of a long long int: %lu byte(s)", sizeof(d));
+printf("Size of a float: %lu byte(s)", sizeof(e));
 return (0);
 }
