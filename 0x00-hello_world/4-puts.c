d4187ad172629aba7b41e273d4929edc2bcc0f02
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-02-16 05:01:57 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/**
+/*
  * main - print a string using puts
  *
  * return:0
@@ -7,5 +7,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
