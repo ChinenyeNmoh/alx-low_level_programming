d4b7386dd2b0b72cf4227bd84aa833196c9b3452
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-02-23 04:37:34 -0800

    4 numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,19 +2,19 @@
 
 /**
  * print_most_numbers - prints number
- *
  * Return: void
  */
 
 void print_most_numbers(void)
 {
-	int i
-		for (i = 0; i <= 9; i++)
+	int i;
+		for (i = 48; i < 58; i++)
+	{
+			if (i != 50 && i != 52)
 		{
-			if (i != 2 && i != 4)
-			{
-				_putchar('0' + i);
-			}
-			_putchar('\n');
+			_putchar(i);
 		}
+	}
+		_putchar('\n');
 }
+
