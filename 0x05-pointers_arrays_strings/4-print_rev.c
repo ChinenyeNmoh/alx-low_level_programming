83272b0db7adffeed355445adcb11f633f9aebd0
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-02-27 05:00:27 -0800

    reverse string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,27 +1,23 @@
 #include "main.h"
 /**
  * rev_string - reverses a string
- * _putchar - print each character
  * @s: char to check
  *
- * Description: This will reverse a string
  * Return: 0 is success
  */
 void rev_string(char *s)
 {
-	int a = 0, b, c;
-	char d;
+	int i = 0, len = 0;
+	char tmp;
 
-	while (s[a] != '\0')
+	while (s[i++])
+		len++;
+
+	for (i = len - 1; i >= len / 2; i--)
 	{
-		a++;
-	}
-	c = a - 1;
-	for (b = 0; c >= 0 && b < c; c--, b++)
-	{
-		d = s[b];
-		s[b] = s[c];
-		s[c] = d;
+		tmp = s[i];
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tmp;
 	}
 }
 
