4feab211b3af595824b9d7dcb1a05a0cdb6a10df
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-03-10 07:23:01 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,10 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int j, i, sum = 0;
+	int i, j, sum = 0;
 
-	if (argc == 1)
-	printf("0\n");
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j]; j++)
@@ -22,10 +20,10 @@ int main(int argc, char *argv[])
 				printf("Error\n");
 				return (1);
 			}
-	}
-	else
+		}
 		sum += atoi(argv[i]);
-}
+	}
 	printf("%d\n", sum);
 	return (0);
 }
+
