76d3f015fb34d7af161d0ff7c95950ad611479ff
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-03-13 13:02:32 -0700

    concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	char *result;
 
 	len1 = len2 = 0;
-	if (s1 && s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (0);
 	while (s1[len1] != '\0')
 		len1++;
