1eee829cc94f7e17ee79b923521196a1f29e351d
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-03-13 10:22:52 -0700

    mallocarray

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 	return ('\0');
-		pt = (*char)malloc(size * sizeof(char));
+		pt = (char*)malloc(size * sizeof(char));
 		if (pt == '\0')
 			return ('\0');
 	for (i = 0; i < size; i++)
