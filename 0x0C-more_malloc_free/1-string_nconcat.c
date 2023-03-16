15e679341d669cf74cd07764503be0f8d4887966
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-03-16 04:31:08 -0700

    strncatmalloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,9 +22,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len1++;
 	while (s2[len2] != '\0')
 		len2++;
-	if (n >= len2)
+	if (n > len2)
 		n = len2;
-	str = (char *)malloc((len1 + n + 1) * sizeof(char));
+	str = malloc((len1 + n + 1) * sizeof(char));
 	if (str == (NULL))
 		return (NULL);
 	for (i = 0; i < len1; i++)
