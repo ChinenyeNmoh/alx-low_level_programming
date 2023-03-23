71367155764bc94e136cdd845564c2fe17c71a46
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-03-23 12:07:09 -0700

    print numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,7 +15,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 	if (separator == NULL)
-		return;
+		separator = "";
 	if (n != 0)
 		for (i = 0; i < n; i++)
 		{
