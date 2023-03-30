3daa699588a23a4985164fcad10f9bde74542ea5
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-03-30 09:03:14 -0700

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+void free_list(list_t *head)
+{
+	list_t *tmp = head;
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+		free(tmp->str);
+		free(tmp);
+	}
+}
