5dd64dd8418b34daa422162cff3880fcca3da040
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-03-30 04:05:44 -0700

    printlist

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - check the code for Holberton School students.
+ * @h: name of the list
+ * Return: the number of nodes.
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *tmp;
+	size_t count = 0;
+
+	if (h == NULL)
+		h = tmp;
+	while (tmp != NULL)
+	{
+		if (tmp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("%d %s\n", tmp->len, tmp->str);
+		}
+		count++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+learning about singly linked list which is a type of data structure
