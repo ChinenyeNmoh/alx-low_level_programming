1295ef09e276fc3ba61ba02b9c2164022914cb3e
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-04-09 09:20:15 -0700

    files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,25 @@
 #include "main.h"
 /**
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+}
+/**
  * main - copies the content of one file to another
  * @argc: argument count
  * @argv: arguments passed
@@ -18,20 +38,16 @@ int main(int argc, char *argv[])
 	}
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_APPEND | O_TRUNC, 0664);
+	error_file(file_from, file_to, argv);
 	while (retr == 1024)
 	{
 		retr = read(file_from, buf, 1024);
-		if (retr == -1 || file_from == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
-	}
+		if (retr == -1)
+			error_file(-1, 0, argv);
+
 	retw = write(file_to, buf, retr);
-	if (retw == -1 || file_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+	if (retw == -1)
+		error_file(0, -1, argv);
 	}
 	closure = close(file_from);
 	if (closure == -1)
