eda67b9af89b48b1d3d7e8e3fa0d32a8ae09a2f4
ChinenyeNmoh <chinenyeumeaku@gmail.com>
2023-05-22 05:23:57 -0700

    python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+ * add - addition opertaion
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * sub - subtraction operation
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * mul - multiplication operation
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * div - division operation
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * mod - modulus operation
+ * @a: number
+ * @b: nuber
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -29,5 +29,4 @@ int sub(int a, int b);
 int mul(int a, int b);
 int mod(int a, int b);
 extern div_t div(int __numer, int __denom);
-
 #endif /* MAIN_H */
